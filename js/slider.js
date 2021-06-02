8e3ffa43c5773a606fd2c92f3f34bf12aae5224b
Serhii <blackdato@gmail.com>
2021-06-02 20:08:41 +0300

    finish hero slider

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.header__slider').slick({
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: false,
+        // centerMode: true
+    });
+});
