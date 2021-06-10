c6c62edaa12ded0e13eda55d7d7669c1ee22d73e
Serhii <blackdato@gmail.com>
2021-06-10 19:17:43 +0300

    fix

--- js/slider.js
+++ js/slider.js
@@ -11,7 +11,36 @@ $(document).ready(function(){
         slidesToShow: 4,
         slidesToScroll: 2,
         draggable: true,
-        rows: 2
+        rows: 2,
+        responsive: [
+          {
+            breakpoint: 1400,
+            settings: {
+              arrows: false,
+              rows: 2,
+              centerMode: false,
+              slidesToShow: 3
+            }
+          },
+            {
+              breakpoint: 1100,
+              settings: {
+                arrows: false,
+                rows: 2,
+                centerMode: true,
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+              }
+            }
+          ]
     });
     $('.staging__slider').slick({
         arrows: false,
@@ -20,6 +49,26 @@ $(document).ready(function(){
         draggable: true,
         variableWidth: true,
         waitForAnimate: true,
-        touchThreshold: 10
+        touchThreshold: 10,
+        // responsive: [
+        //   {
+        //     breakpoint: 1400,
+        //     settings: {
+        //       arrows: false,
+        //       centerMode: true,
+        //       variableWidth: false,
+        //       slidesToShow: 3
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 480,
+        //     settings: {
+        //       arrows: false,
+        //       centerMode: true,
+        //       centerPadding: '40px',
+        //       slidesToShow: 1
+        //     }
+        //   }
+        // ]
     });
 });
